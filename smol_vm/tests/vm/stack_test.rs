592abc4941a38f856f36bc2421227b0a16edd34b
Miika Alikirri <nykseli@protonmail.com>
2023-07-04 20:59:57 +0300

    vm: added tests for popping and pushing stack

--- smol_vm/tests/vm/stack_test.rs
+++ smol_vm/tests/vm/stack_test.rs
@@ -81,3 +81,97 @@ pub fn it_resets_stackpointer() {
     vm.run();
     assert_eq!(vm.registers.sp, 123);
 }
+
+#[test]
+pub fn it_pushes_register() {
+    let mut vm = Vm::default();
+    vm.registers.r3 = 5;
+    vm.instructions.instructions = vec![
+        // PUR  r/8  - Push register onto stack
+        0b10_00_0_0_00,
+        // Register r3
+        0b0000_0011,
+    ];
+    vm.run();
+    assert_eq!(vm.registers.sp, 1);
+    assert_eq!(vm.stack.memory()[0], 5);
+}
+
+#[test]
+pub fn it_pushes_register_16b() {
+    let mut vm = Vm::default();
+    vm.registers.l0 = 258;
+    vm.instructions.instructions = vec![
+        // PURL r/16 - Push 16-bit register onto stack
+        0b10_00_0_1_00,
+        // Register l0
+        0b0000_1001,
+    ];
+    vm.run();
+    assert_eq!(vm.registers.sp, 2);
+    assert_eq!(vm.stack.memory()[0], 2);
+    assert_eq!(vm.stack.memory()[1], 1);
+}
+
+#[test]
+pub fn it_pushes_immidiate() {
+    let mut vm = Vm::default();
+    vm.instructions.instructions = vec![
+        // PUI  i/8  - Push immidiate onto stack
+        0b10_00_1_0_00,
+        5,
+    ];
+    vm.run();
+
+    assert_eq!(vm.registers.sp, 1);
+    assert_eq!(vm.stack.memory()[0], 5);
+}
+
+#[test]
+pub fn it_pushes_immidiate_16b() {
+    let mut vm = Vm::default();
+    vm.instructions.instructions = vec![
+        // PURL i/16 - Push 16-bit immidiate onto stack
+        0b10_00_1_1_00,
+        2,
+        1,
+    ];
+    vm.run();
+    assert_eq!(vm.registers.sp, 2);
+    assert_eq!(vm.stack.memory()[0], 2);
+    assert_eq!(vm.stack.memory()[1], 1);
+}
+
+#[test]
+pub fn it_pops() {
+    let mut vm = Vm::default();
+    vm.registers.sp = 1;
+    vm.stack.memory_mut()[0] = 5;
+    vm.instructions.instructions = vec![
+        // POR  r/8  - Pop stack into register
+        0b10_01_0_0_00,
+        2,
+    ];
+    vm.run();
+
+    assert_eq!(vm.registers.sp, 0);
+    assert_eq!(vm.registers.r2, 5);
+}
+
+#[test]
+pub fn it_pops_16b() {
+    let mut vm = Vm::default();
+    vm.registers.sp = 2;
+    vm.stack.memory_mut()[0] = 2;
+    vm.stack.memory_mut()[1] = 1;
+    vm.instructions.instructions = vec![
+        // PORL  r/16  - Pop stack into 16-bit register
+        0b10_01_0_1_00,
+        // Register l0
+        0b1001,
+    ];
+    vm.run();
+
+    assert_eq!(vm.registers.sp, 0);
+    assert_eq!(vm.registers.l0, 258);
+}
